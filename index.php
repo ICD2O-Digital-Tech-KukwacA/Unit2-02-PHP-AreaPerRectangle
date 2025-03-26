3b46925cea1cb90f9123ac4aa22aa21ad86de86b
Kukwac Adiebo <kukwac.adiebo@stu.ocsb.ca>
2025-03-26 22:20:35 +0000

    Added comments to the code

--- index.php
+++ index.php
@@ -1,18 +1,23 @@
+// Unit2-02-PHP-AreaPerRectangle
 <!DOCTYPE html>
 <html lang="en">
 	<head>
+// This is the metadata
     <meta charset="utf-8">
     <meta name="description" content="My first webpage, with JavaScript">
     <meta name="keywords" content="immaculata, ics2o">
     <meta name="author" content="Ms Raffin">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+// This is the section for Favicons
     <link rel="apple-touch-icon" sizes="180x180" href="./Favicons/apple-touch-icon.png">
     <link rel="icon" type="image/png" sizes="32x32" href="./Favicons/favicon-32x32.png">
     <link rel="icon" type="image/png" sizes="16x16" href="./Favicons/favicon-16x16.png">
     <link rel="manifest" href="./Favicons/site.webmanifest">
+// This is the stylesheet
     <link rel="stylesheet" href="./css/style.css">
     <title>Area & Perimeter of Rectangle in PHP</title>
 	</head>
+// This section contains the web page content
 	<body>
     <h1><?php echo 'Area & Perimeter of a Rectangle'; ?></h1>
 		<?php 
